64b431a3461d2005c25510ca5536ebc9a6c002a4
Miriam Gold <goldm@uchicago.edu>
2023-12-08 10:47:28 -0600

    add comments and documentation to oco2 download file

--- code/oco2/oco2_data_download.sh
+++ code/oco2/oco2_data_download.sh
@@ -1,8 +1,23 @@
+#############################################
+# Description: Download OCO2 data from NASA
+# Author: Miriam Gold
+# Date: 5 Dec 2023
+# Last revised: 8 Dec 2023
+# Notes: 
+#############################################
+
+
 #!/bin/bash
+
+# Set location of beavers project directory on local machine 
+cd "C:\Users\mGold\Desktop\beavers"
+
+# Read in all file paths for downloading
 input="$PWD\data\helper_data\subset_OCO2_L2_Lite_FP_11.1r_20231206_042328_.txt"
 mapfile FILES < $input
 
-for fileurl in ${FILES[0]}; 
+# Download each file 
+for fileurl in ${FILES[@]}; 
 do
     filename=$(basename $fileurl)
 
